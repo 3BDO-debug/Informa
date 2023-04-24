2a1e0f88dd964d1ba5de84c3f86b2b43da45f297
code-hustler-725 <rooted39@gmail.com>
2023-04-24 22:32:36 +0200

    Quickly switched to pythonanywhere

--- src/__apis__/axios.js
+++ src/__apis__/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// http://127.0.0.1:8000  https://informa-server.herokuapp.com/ https://informa-server-v2.herokuapp.com/
+// http://127.0.0.1:8000  https://informa-server.herokuapp.com/ https://informa-server-v2.herokuapp.com/ https://informa180.pythonanywhere.com/
 
-export const mainUrl = 'https://informa-server-v2.herokuapp.com';
+export const mainUrl = 'https://informa180.pythonanywhere.com/';
 
 const axiosInstance = axios.create({
   baseURL: mainUrl,
