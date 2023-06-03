8e9b23987e97ab3f8309252a60d9630fda23d213
code-hustler-725 <rooted39@gmail.com>
2023-06-03 14:16:57 +0300

    Switched to prod

--- src/__apis__/axios.js
+++ src/__apis__/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // http://127.0.0.1:8000  https://informa-server.herokuapp.com/ https://informa-server-v2.herokuapp.com/ https://informa180.pythonanywhere.com/
 
-export const mainUrl = 'http://127.0.0.1:8000';
+export const mainUrl = 'https://informa-server-v2.herokuapp.com';
 
 const axiosInstance = axios.create({
   baseURL: mainUrl,
