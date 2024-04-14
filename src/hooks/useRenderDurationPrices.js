7776a4da7045104b1108aad47ad4ffe169d02f5a
Abdelrahman Essam <64382556+3BDO-debug@users.noreply.github.com>
2024-04-14 04:27:37 +0200

    updated pricings

--- src/hooks/useRenderDurationPrices.js
+++ src/hooks/useRenderDurationPrices.js
@@ -21,26 +21,26 @@ function useRenderDurationPrices() {
         {
           value: 1,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.1'),
-          egpPrice: `1500 L.E`,
-          usdPrice: `105 $`,
+          egpPrice: `1,750 L.E`,
+          usdPrice: `125 $`,
         },
         {
           value: 3,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.2'),
-          egpPrice: `3000 L.E`,
-          usdPrice: `210 $`,
+          egpPrice: `3,500 L.E`,
+          usdPrice: `250 $`,
         },
         {
           value: 6,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.3'),
-          egpPrice: `5000 L.E`,
-          usdPrice: `350 $`,
+          egpPrice: `6,000 L.E`,
+          usdPrice: `420 $`,
         },
         {
           value: 12,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.4'),
-          egpPrice: `8000 L.E`,
-          usdPrice: `560 $`,
+          egpPrice: `9,500 L.E`,
+          usdPrice: `670 $`,
         },
       ]);
     } else if (userPlan.program === 'nutrition') {
@@ -48,44 +48,44 @@ function useRenderDurationPrices() {
         {
           value: 1,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.1'),
-          egpPrice: `1150 L.E`,
-          usdPrice: `75 $`,
+          egpPrice: `1,500 L.E`,
+          usdPrice: `90 $`,
         },
         {
           value: 3,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.2'),
-          egpPrice: `2300 L.E`,
-          usdPrice: `150 $`,
+          egpPrice: `3,000 L.E`,
+          usdPrice: `180 $`,
         },
         {
           value: 6,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.3'),
-          egpPrice: `3750 L.E`,
-          usdPrice: `245 $`,
+          egpPrice: `4,500 L.E`,
+          usdPrice: `300 $`,
         },
-        { value: 12, label: '12 Months', egpPrice: `6000 L.E`, usdPrice: `400 $` },
+        { value: 12, label: '12 Months', egpPrice: `7,000 L.E`, usdPrice: `480 $` },
       ]);
     } else if (userPlan.program === 'workout') {
       setPricesList([
         {
           value: 1,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.1'),
-          egpPrice: `900 L.E`,
-          usdPrice: `60 $`,
+          egpPrice: `1,000 L.E`,
+          usdPrice: `70 $`,
         },
         {
           value: 3,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.2'),
-          egpPrice: `1800 L.E`,
-          usdPrice: `120 $`,
+          egpPrice: `2,000 L.E`,
+          usdPrice: `140 $`,
         },
         {
           value: 6,
           label: translate('commonSectionsTranslations.pricingsSection.planDuration.3'),
-          egpPrice: `3000 L.E`,
-          usdPrice: `200 $`,
+          egpPrice: `3,500 L.E`,
+          usdPrice: `240 $`,
         },
-        { value: 12, label: '12 Months', egpPrice: `5000 L.E`, usdPrice: `350 $` },
+        { value: 12, label: '12 Months', egpPrice: `6,000 L.E`, usdPrice: `420 $` },
       ]);
     }
   }, [userPlan.program, translate, currentLang]);
