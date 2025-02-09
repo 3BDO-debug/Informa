d49a7c6f78404cd3f4be9f1a1c4265d650ffcc50
Abdelrahman Essam <64382556+3BDO-debug@users.noreply.github.com>
2025-02-09 21:19:46 +0200

    Fixed layout issue

--- src/__apis__/axios.js
+++ src/__apis__/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// http://127.0.0.1:8000 ///  https://informa-2f385573ab19.herokuapp.com https://informa180.pythonanywhere.com/ ....
+// http://127.0.0.1:8000 //  https://informa-2f385573ab19.herokuapp.com https://informa180.pythonanywhere.com/ ....
 
-export const mainUrl = 'http://127.0.0.1:8000';
+export const mainUrl = 'https://informa-2f385573ab19.herokuapp.com';
 
 const axiosInstance = axios.create({
   baseURL: mainUrl,
