7c731abc44095f6db68ebe7659ef6d029abf5903
Abdelrahman Ali <aliboody8@gmail.com>
2025-04-14 15:36:58 +0200

    checkout transformations edit

--- src/components/TransformationCheckout.js
+++ src/components/TransformationCheckout.js
@@ -8,32 +8,42 @@ function TransformationCheckout() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
   };
 
-  const images = [{ img: '2.png' }, { img: '3.png' }, { img: '4.png' }];
+  const images = [
+    { img: '2.png' },
+    { img: '3.png' },
+    { img: '4.png' },
+    { img: '5.png' },
+    { img: '6.png' },
+    { img: '7.png' },
+    { img: '8.png' },
+    { img: '9.png' },
+    { img: '10.png' },
+  ];
 
   return (
     <Box sx={{ position: 'relative', minHeight: 300 }}>
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box sx={{ width: { xs: 200, md: 400 } }} component="img" src="/images/checkout-transformations/1.png" />
+        <Box sx={{ width: { xs: 250, md: 400 } }} component="img" src="/images/checkout-transformations/1.png" />
       </Box>
-      <Box sx={{ mt: { xs: -19, md: -27 } }}>
+      <Box sx={{ mt: { xs: -3, md: -4 } }}>
         <Slider {...settings}>
           {images.map((_, index) => (
-            <Box>
+            <Box sx={{ marginX: 0 }}>
               <Box
                 sx={{
                   height: {
-                    xs: 300,
-                    md: 400,
+                    xs: 110,
+                    md: 310,
                   },
                   aspectRatio: '1/1',
                   width: '100%',
-                  objectFit: 'cover',
+                  objectFit: 'contain',
                 }}
                 component="img"
                 src={`/images/checkout-transformations/${_.img}`}
