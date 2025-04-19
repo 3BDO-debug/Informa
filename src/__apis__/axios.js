61aaae3d0dbf42aabbce5e924e420d4a8f310d9a
3bdo-debug <rooted39@gmail.com>
2025-04-19 14:54:22 +0200

    updated remote url

--- src/__apis__/axios.js
+++ src/__apis__/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// http://127.0.0.1:8000 2--/-33=-3/https://if180-375381fc460c.herokuapp.com 33  https://informa-2f385573ab19.herokuapp.com https://informa180.pythonanywhere.com/ ....
+// http://127.0.0.1:8000  https://informa-7034db3559df.herokuapp.com --/-33=-3/https://if180-375381fc460c.herokuapp.com 33  https://informa-2f385573ab19.herokuapp.com https://informa180.pythonanywhere.com/ ....
 
-export const mainUrl = 'https://if180-375381fc460c.herokuapp.com';
+export const mainUrl = 'https://informa-7034db3559df.herokuapp.com';
 
 const axiosInstance = axios.create({
   baseURL: mainUrl,
