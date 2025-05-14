fd0e19b3f33b3f52146b144615afba65308e5fee
3bdo-debug <rooted39@gmail.com>
2025-05-15 02:34:16 +0300

    __

--- src/__apis__/axios.js
+++ src/__apis__/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// http://127.0.0.1:8000  https://informa-7034db3559df.herokuapp.com --/-33=-3/https://if180-375381fc460c.herokuapp.com 33  https://informa-2f385573ab19.herokuapp.com https://informa180.pythonanywhere.com/ ....
+// http://127.0.0.1:8000  https://informa-7034db3559df.herokuapp.com --/2-33=-3/https://if180-375381fc460c.herokuapp.com 33  https://informa-2f385573ab19.herokuapp.com https://informa180.pythonanywhere.com/ ....
 
 export const mainUrl = 'https://informa-7034db3559df.herokuapp.com';
 
